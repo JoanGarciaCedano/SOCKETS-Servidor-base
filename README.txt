0c4a836edb1eb115fc61518201cd0542d798e4c5
JoanGC <joan.gc.mail@gmail.com>
2018-12-07 16:18:16 -0700

    Primer commit - Rest Server

---
+++ README.txt
@@ -0,0 +1,7 @@
+Paquetes instalados en el proyecto:
+
+1.- ExpressJs
+2.- CORS
+3.- BodyParser
+
+Creamos nuestro servidor Express:
---
+++ index.ts
@@ -0,0 +1,23 @@
+import Server from './classes/server';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+const server = new Server();
+
+//Configuramos el bodyParser para recibir datos
+//parse application/x-www-form-urlencoded
+server.app.use(bodyParser.urlencoded({extended : true}));
+//parse application/json
+server.app.use(bodyParser.json());
+
+//CORS - configuración del CORS para el acceso desde diferentes hostings
+server.app.use( cors({ origin: true, credentials: true}));
+
+//Rutas de servicios
+server.app.use('/', router);
+
+server.start( () => {
+    console.log(`Servidor corriendo en el puerto ${ server.port}`);
+});
